f419adcb65c9acb6a182b06d87c514ec768ab0be
Karyna_Taranova <Karyna_Taranova@epam.com>
2019-01-29 09:53:04 +0200

    add ptai_model class

---
+++ dusty/data_model/ptai_model.py
@@ -0,0 +1,35 @@
+from dusty.data_model.canonical_model import DefaultModel
+
+
+class PTAIModel(DefaultModel):
+    def __init__(self, title, severity, description, tool, endpoints=None,
+                 scanner_confidence=None, static_finding=None, dynamic_finding=None,
+                 impact=None, mitigation=None, date=None, cwe=None, url=None,
+                 steps_to_reproduce=None, severity_justification=None,
+                 references=None, images=None, line_number=None,
+                 sourcefilepath=None, sourcefile=None, param=None,
+                 payload=None, line=None, file_path=None, comments=None,
+                 **kwags):
+        super().__init__(title, severity, description, tool, endpoints,
+                 scanner_confidence, static_finding, dynamic_finding,
+                 impact, mitigation, date, cwe, url,
+                 steps_to_reproduce, severity_justification,
+                 references, images, line_number,
+                 sourcefilepath, sourcefile, param,
+                 payload, line, file_path,
+                 **kwags)
+        self.finding['description'] = description
+        self.finding['title'] = title
+        self.finding['comments'] = comments
+        self.scan_type = 'SAST'
+
+    def __str__(self):
+        return self.finding['description']
+
+    def jira(self, jira_client):
+        issue = super().jira(jira_client)
+        print(issue.key)
+        if self.finding['comments']:
+            for data in self.finding['comments']:
+                jira_client.add_comment_to_issue(issue, data)
+        return issue
