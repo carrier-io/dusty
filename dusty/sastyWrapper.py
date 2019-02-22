c8946acc72e48ae65b40a4eaf07e8573b8b4d7c0
karynataranova <karina.taranova@gmail.com>
2019-02-22 17:11:24 +0200

    add parallel run

--- dusty/sastyWrapper.py
+++ dusty/sastyWrapper.py
@@ -31,6 +31,42 @@ class SastyWrapper(object):
         return config.get("code_path", constants.PATH_TO_CODE)
 
     @staticmethod
+    def python(config):
+        exec_cmd = "bandit -r {} --format json".format(SastyWrapper.get_code_path(config))
+        res = execute(exec_cmd, cwd=SastyWrapper.get_code_path(config))
+        with open("/tmp/bandit.json", "w") as f:
+            f.write(res[0].decode('utf-8', errors='ignore'))
+        result = BanditParser("/tmp/bandit.json", "pybandit").items
+        filtered_result = common_post_processing(config, result, "pybandit")
+        return filtered_result
+
+    @staticmethod
+    def ruby(config):
+        included_checks = ''
+        exclude_checks = ''
+        if config.get('include_checks', None):
+            included_checks = f'-t {config.get("include_checks")} '
+        if config.get('exclude_checks', None):
+            exclude_checks = f'-x {config.get("exclude_checks")} '
+        if config.get('excluded_files', None):
+            exclude_checks = f'--skip-files {config.get("excluded_files")} '
+        excluded_files = ''
+        exec_cmd = f"brakeman {included_checks}{exclude_checks}--no-exit-on-warn --no-exit-on-error {excluded_files}" \
+                       f"-o /tmp/brakeman.json " + SastyWrapper.get_code_path(config)
+        execute(exec_cmd, cwd=SastyWrapper.get_code_path(config))
+        result = BrakemanParser("/tmp/brakeman.json", "brakeman").items
+        filtered_result = common_post_processing(config, result, "brakeman")
+        return filtered_result
+
+    @staticmethod
+    def java(config):
+        exec_cmd = "spotbugs -xml:withMessages -output /tmp/spotbugs.xml {}".format(SastyWrapper.get_code_path(config))
+        res = execute(exec_cmd, cwd=SastyWrapper.get_code_path(config))
+        result = SpotbugsParser("/tmp/spotbugs.xml", "spotbugs").items
+        filtered_result = common_post_processing(config, result, "spotbugs")
+        return filtered_result
+
+    @staticmethod
     def nodejs(config):
         scan_fns = [SastyWrapper.nodejsscan]
         composition_analysis = config.get('composition_analysis',None)
@@ -48,19 +84,6 @@ class SastyWrapper(object):
         for result in results:
             all_results.extend(result)
         return all_results
-
-    @staticmethod
-    def bandit(config, results=None):
-        exec_cmd = "bandit -r {} --format json".format(SastyWrapper.get_code_path(config))
-        res = execute(exec_cmd, cwd=SastyWrapper.get_code_path(config))
-        with open("/tmp/bandit.json", "w") as f:
-            f.write(res[0].decode('utf-8', errors='ignore'))
-        result = BanditParser("/tmp/bandit.json", "pybandit").items
-        filtered_result = common_post_processing(config, result, "pybandit")
-        if results:
-            results.append(filtered_result)
-        else:
-            return filtered_result
 
     @staticmethod
     def npm(config, results=None):
@@ -93,32 +116,6 @@ class SastyWrapper(object):
             results.append(filtered_result)
         else:
             return filtered_result
-
-    @staticmethod
-    def ruby(config):
-        included_checks = ''
-        exclude_checks = ''
-        if config.get('include_checks', None):
-            included_checks = f'-t {config.get("include_checks")} '
-        if config.get('exclude_checks', None):
-            exclude_checks = f'-x {config.get("exclude_checks")} '
-        if config.get('excluded_files', None):
-            exclude_checks = f'--skip-files {config.get("excluded_files")} '
-        excluded_files = ''
-        exec_cmd = f"brakeman {included_checks}{exclude_checks}--no-exit-on-warn --no-exit-on-error {excluded_files}" \
-                   f"-o /tmp/brakeman.json " + SastyWrapper.get_code_path(config)
-        execute(exec_cmd, cwd=SastyWrapper.get_code_path(config))
-        result = BrakemanParser("/tmp/brakeman.json", "brakeman").items
-        filtered_result = common_post_processing(config, result, "brakeman")
-        return filtered_result
-    
-    @staticmethod
-    def java(config):
-        exec_cmd = "spotbugs -xml:withMessages -output /tmp/spotbugs.xml {}".format(SastyWrapper.get_code_path(config))
-        res = execute(exec_cmd, cwd=SastyWrapper.get_code_path(config))
-        result = SpotbugsParser("/tmp/spotbugs.xml", "spotbugs").items
-        filtered_result = common_post_processing(config, result, "spotbugs")
-        return filtered_result
 
     @staticmethod
     def nodejsscan(config, results):
