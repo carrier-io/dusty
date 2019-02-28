942a14e0456ee63f7c254b42b9e91d69e4ff5119
Ivan Krakhmaliuk <lifedj27@gmail.com>
2019-02-28 12:07:35 +0200

    Output loaded config path

--- dusty/run.py
+++ dusty/run.py
@@ -113,11 +113,13 @@ def config_from_yaml():
     email_attachments = []
     if constants.CONFIG_ENV_KEY in os.environ.keys():
         config = yaml.load(os.environ.get(constants.CONFIG_ENV_KEY))
+        print(f"Loaded configuration from ${constants.CONFIG_ENV_KEY}")
     else:
         path_to_config = os.environ.get('config_path', constants.PATH_TO_CONFIG)
-        path_to_false_positive = os.environ.get('false_positive_path', constants.FALSE_POSITIVE_CONFIG)
         with open(path_to_config, "rb") as f:
             config = yaml.load(f.read())
+            print(f"Loaded configuration from ${path_to_config}")
+    path_to_false_positive = os.environ.get('false_positive_path', constants.FALSE_POSITIVE_CONFIG)
     suites = list(config.keys())
     args = arg_parse(suites)
     test_name = args.suite
