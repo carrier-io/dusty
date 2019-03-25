11c3bf8a935706a617bd4b71d29d0a0e675a5557
arozumenko <artyom.rozumenko@gmail.com>
2019-03-25 19:50:01 +0200

    modified influx storage schema for demo purpose only
    Signed-off-by: arozumenko <artyom.rozumenko@gmail.com>

--- dusty/drivers/influx.py
+++ dusty/drivers/influx.py
@@ -1,4 +1,5 @@
 import datetime
+from time import time
 
 from influxdb import InfluxDBClient
 from dusty.utils import define_jira_priority
@@ -10,6 +11,7 @@ class InfluxReport(object):
         self.port = default_config['influx'].get('port', 8086)
         self.db = default_config['influx'].get('db', 'prodsec')
         self.login = default_config['influx'].get('login', '')
+        self.policy = default_config['influx'].get('policy', {'Blocker': 1, 'Critical': 5, 'Major': 15})
         self.password = default_config['influx'].get('password', '')
         self.project_name = default_config['project_name']
         self.environment = default_config['project_name']
@@ -21,6 +23,8 @@ class InfluxReport(object):
         self.results_by_severity = self.sort_results_by_severity()
         self.results_by_severity['new_in_jira'] = self.get_new_jira_issues()
         self.results_by_severity['total_in_jira'] = self.open_issues
+        self.results_by_severity['test_to_count'] = 1
+        self.build_id = f'{self.execution_time} - {self.project_name}'
         self.client = InfluxDBClient(self.host, self.port, username=self.login,
                                      password=self.password, database=self.db)
         self._ingest_active_errors()
@@ -49,39 +53,45 @@ class InfluxReport(object):
         pass
 
     def _ingest_active_errors(self):
-        stats = {
+        stats = [{
             "measurement": "stats",
             "time": self.execution_time,
             "tags": {
+                'build_id': self.build_id,
                 'test_name': self.test_type,
                 'type': self.test_type,
                 'project': self.project_name
             },
             "fields": self.results_by_severity
-        }
-        self.client.write_points([stats])
+        }]
+        self.client.write_points(stats)
         jira_issues = []
         for issue in self.created_jira_tickets:
+            ts = int(datetime.datetime.strptime(issue['open_date'], '%Y-%m-%dT%H:%M:%S.%f%z').timestamp())
+            break_policy = 'Y' if ts + (self.policy[str(issue['priority'])]*24*3600) < int(time()) else 'N'
             issue = {
                 "measurement": "errors",
                 "time": self.execution_time,
                 "tags": {
+                    'build_id': self.build_id,
+                    'description': str(issue['description']),
                     'test_name': self.test_type,
                     'type': self.test_type,
-                    'project': self.project_name
+                    'project': self.project_name,
+                    'priority': issue['priority'],
+                    'created': datetime.datetime.strptime(
+                        issue['open_date'], '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%d %b %Y %H:%M:%S.%f'),
+                    'link': str(issue['link'])
                 },
                 "fields": {
-                    'priority': issue['priority'],
-                    'status': issue['status'],
-                    'link': issue['link'],
-                    'created': datetime.datetime.strptime(issue['open_date'],
-                                                          '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%d %b %Y %H:%M'),
-                    'description': issue['description'],
-                    'assignee': issue['assignee']
+                    'breaking_policy': break_policy,
+                    'status': str(issue['status']),
+                    'assignee': str(issue['assignee']),
+                    'quantity': 1
                 }
             }
             jira_issues.append(issue)
-        self.client.write_points(stats)
+        self.client.write_points(jira_issues)
 
 
 
